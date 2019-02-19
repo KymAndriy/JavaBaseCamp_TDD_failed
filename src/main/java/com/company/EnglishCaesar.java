c02a36dd8ed793baa9850734a85ed6e6173215dd
Андрій Михаш <capitaw8@gmail.com>
2019-02-19 21:28:03 +0200

    I have problem with naming commits

--- src/main/java/com/company/EnglishCaesar.java
+++ src/main/java/com/company/EnglishCaesar.java
@@ -2,11 +2,13 @@ package com.company;
 
 public class EnglishCaesar {
 
+
+
     public boolean isLowerCase(String check){
         return (check.compareTo(check.toLowerCase()) == 0)? true: false;
     }
 
-    public String caesarCipher(String romaine_lettce, int i) {
-        return "";
+    public String caesarCipher(String originalText, int stepOfChange) {
+       return "";
     }
 }
--- src/test/java/com/test/CaesarTest.java
+++ src/test/java/com/test/CaesarTest.java
@@ -4,23 +4,25 @@ import com.company.EnglishCaesar;
 import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CaesarTest {
 
-    EnglishCaesar unit;
+    EnglishCaesar unit = new EnglishCaesar();
 
-    @Before
-    void beforeTest(){
-        EnglishCaesar unit = new EnglishCaesar();
+
+    @Test
+    public void whenStringUpperReturnFalse(){
+        assertTrue(unit.isLowerCase("ksjlhkfalkf"));
     }
 
     @Test
-    public void whenStringUpperReturnFalseAndLowerReturnFalse(){
-        assertTrue(!unit.isLowerCase("KUGKJHGKG"));
-        assertTrue(unit.isLowerCase("ksjlhkfalkf"));
+    public void whenTakeUpperOrMixedCaseStringMakeItLower(){
+        assertEquals("Yess, someone made it",unit.caesarCipher("sjkaskdh", 9));
+
     }
 
     @Test
@@ -28,5 +30,6 @@ public class CaesarTest {
 
         assertEquals("axvjrwn unccdln",unit.caesarCipher("romaine lettce", 9));
     }
+    
 
 }
